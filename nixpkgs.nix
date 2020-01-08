f8a94f2f0f977ceec4ec10842d0b4c762b222396
Tyler Benster <tbenst@gmail.com>
2020-01-08 10:25:59 -0800

    update nixpkgs and add cereal

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs-channels/archive/91b286c8935b8c5df4a99302715200d3bd561977.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs-channels/archive/b0bbacb52134a7e731e549f4c0a7a2a39ca6b481.tar.gz";
   # nix-prefetch-url https://github.com/NixOS/nixpkgs-channels/archive/91b286c8935b8c5df4a99302715200d3bd561977.tar.gz --unpack
-  sha256 = "1c4a31s1i95cbl18309im5kmswmkg91sdv5nin6kib2j80gixgd3";
+  sha256 = "15ix4spjpdm6wni28camzjsmhz0gzk3cxhpsk035952plwdxhb67";
 })
